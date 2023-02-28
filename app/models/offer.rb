2b3c1059c89d772c7f1714b264b7f230c34cc676
Thibault Mercier <thib.mercier@ymail.com>
2023-02-28 16:30:47 +0100

    cloudinary done

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many_attached :photos
+  has_one_attached :photo
   validates :name, :category, :description, :address, :price_per_day, presence: true
   validates :category, inclusion: { in: ["Baby-sitting/Garde d'enfants", "Maison/Décoration", "Réparation/Entretien", "Dog-sitting/Garde d'animaux", "Jardin/Extérieur", "Animation", "Traiteur/Service à table" ] }
   validates :name, length: { minimum: 3 }
