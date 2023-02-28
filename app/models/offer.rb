e1f46a5417443605e82f9d02923b43459351a432
Thibault Mercier <thib.mercier@ymail.com>
2023-02-28 15:13:19 +0100

    offers_new done avec ajout de validations dans model Offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,11 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
+  validates :name, :category, :description, :address, :price_per_day, presence: true
+  validates :category, inclusion: { in: ["Baby-sitting/Garde d'enfants", "Maison/Décoration", "Réparation/Entretien", "Dog-sitting/Garde d'animaux", "Jardin/Extérieur", "Animation", "Traiteur/Service à table" ] }
+  validates :name, length: { minimum: 3 }
+  validates :description, length: { minimum: 6 }
+
+  CATEGORY = ["Baby-sitting/Garde d'enfants", "Maison/Décoration", "Réparation/Entretien", "Dog-sitting/Garde d'animaux", "Jardin/Extérieur", "Animation", "Traiteur/Service à table" ]
 end
