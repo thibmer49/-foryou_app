7ee2d9843cf52f5bb7d02b7bab572b79971fad77
Thibault Mercier <thib.mercier@ymail.com>
2023-03-01 16:24:56 +0100

    tt

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,20 @@ require 'faker'
 #   Character.create(name: "Luke", movie: movies.first)
 puts "Cleaning up database..."
 
-# User.destroy_all
-# 10.times do
-#   User.create!(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     pseudo: Faker::Name.name,
-#     email: Faker::Internet.email,
-#     address: Faker::Address.full_address,
-#     password: Faker::Internet.password,
-#     phone_number: Faker::PhoneNumber.phone_number_with_country_code,
-#     birth_date: Faker::Date.birthday
-#   )
-# end
+# DO NOT REMOVE
+User.destroy_all
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    pseudo: Faker::Name.name,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address,
+    password: Faker::Internet.password,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    birth_date: Faker::Date.birthday
+  )
+end
 Offer.destroy_all
 puts "Database cleaned"
 12.times do
