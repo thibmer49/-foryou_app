fdde342b42206108715e6d491a44c36120547bc0
Til Lombardo <til.lombardo@hotmail.fr>
2023-03-03 12:20:34 +0100

    total price

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :offer
   belongs_to :user
-  #test
-end
+  before_save :calculate_duration
 
+  private
+
+  def calculate_duration
+    self.duration = (end_date - start_date).to_i
+  end
+end
